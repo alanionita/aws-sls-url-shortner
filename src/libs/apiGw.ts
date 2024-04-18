da41b87bc0a606b402dbd0d0f510b9a67d292b2e
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2024-04-18 12:09:24 +0100

    FEAT (libs) : adds api gw lib

---
+++ src/libs/apiGw.ts
@@ -0,0 +1,12 @@
+interface IFormatJSONResponseInput { statusCode?: number, data?: any }
+
+export function formatJSONResponse({ statusCode = 200, data = {} }: IFormatJSONResponseInput) {
+    return {
+        statusCode,
+        body: JSON.stringify(data),
+        headers: {
+            'Access-Control-Allow-Origin': "*",
+            'Access-Control-Allow-Credentials': true
+        }
+    }
+}
