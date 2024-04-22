bab60ef0554041930353c95d5017eb7aa711e631
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2024-04-22 12:06:35 +0100

    FEAT (libs/apiGw) : updates formatJSON to handle headers

--- src/libs/apiGw.ts
+++ src/libs/apiGw.ts
@@ -1,12 +1,13 @@
-interface IFormatJSONResponseInput { statusCode?: number, data?: any }
+interface IFormatJSONResponseInput { statusCode?: number, data?: any, headers?: Record<string, string> }
 
-export function formatJSONResponse({ statusCode = 200, data = {} }: IFormatJSONResponseInput) {
+export function formatJSONResponse({ statusCode = 200, data = {}, headers = {}}: IFormatJSONResponseInput) {
     return {
         statusCode,
         body: JSON.stringify(data),
         headers: {
             'Access-Control-Allow-Origin': "*",
-            'Access-Control-Allow-Credentials': true
+            'Access-Control-Allow-Credentials': true,
+            ...headers
         }
     }
 }
