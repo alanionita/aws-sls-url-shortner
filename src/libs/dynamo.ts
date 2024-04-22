485b90e80090995dd9709163a54bf2fde167ba11
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2024-04-22 11:49:10 +0100

    FEAT (libs/dynamo) : adds get util for fetching items from dynamo

--- src/libs/dynamo.ts
+++ src/libs/dynamo.ts
@@ -1,5 +1,5 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { PutCommand, PutCommandInput } from "@aws-sdk/lib-dynamodb"
+import { GetCommand, GetCommandInput, PutCommand, PutCommandInput } from "@aws-sdk/lib-dynamodb"
 
 const ddbClient = new DynamoDBClient({});
 
@@ -16,5 +16,20 @@ export const dynamo = {
         await ddbClient.send(command);
 
         return data;
+    },
+    get: async (id: string, tableName: string) => {
+        
+        const getParams: GetCommandInput = {
+            TableName: tableName,
+            Key: {
+                id: id
+            }
+        };
+        
+        const command = new GetCommand(getParams);
+
+        const res = await ddbClient.send(command);
+
+        return res.Item;
     }
 }
