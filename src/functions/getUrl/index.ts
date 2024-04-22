1c0d2c4a093dff05f1372d2316ce595d5a6b52f3
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2024-04-22 11:49:57 +0100

    FEAT (sls): adds new lambda for getting the url
    Implements handler for getting the data from DynamoDB using the shortcode (PK)
    
    Also adds some error handling for when the code is missing

---
+++ src/functions/getUrl/index.ts
@@ -0,0 +1,42 @@
+import { formatJSONResponse } from "@libs/apiGw";
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { dynamo } from "@libs/dynamo";
+
+export async function handler(event: APIGatewayProxyEvent) {
+    try {
+        const tableName = process.env?.URL_TABLE;
+        // const baseUrl = process.env?.BASE_URL;
+
+        if (!tableName) throw Error('Table name value cannot be found!')
+
+        const { code } = event.pathParameters || {};
+
+        if (!code) {
+            return formatJSONResponse({
+                statusCode: 400,
+                data: {
+                    message: 'Missing code in path'
+                }
+            })
+        }
+
+        const record = await dynamo.get(code, tableName);
+
+        const { url } = record || {}
+
+        return formatJSONResponse({
+            data: {
+                url
+            }
+        })
+    } catch (err) {
+        console.error(err.message)
+        console.info(JSON.stringify(err.stack))
+        return formatJSONResponse({
+            statusCode: 502,
+            data: {
+                message: err.message
+            }
+        })
+    }
+}
